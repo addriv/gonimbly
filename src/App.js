573b37cf9d58767a627a6c47d87904a721b55db4
addriv <adrivero89@gmail.com>
2018-01-31 19:20:31 -0800

    Finish user inputs

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      type: 1,
+      name: 'Robot',
+      set: 1,
       width: 100,
       height: 100,
       background: 0
@@ -20,18 +20,34 @@ class App extends Component {
     };
   }
 
+  nameError() {
+    return (
+      <div className="name-error">Your robot needs a name!</div>
+    );
+  }
+
   render() {
-    const { name, type, width, height, background } = this.state;
-    const uri =  `https://robohash.org/${name}?set=set${type}&size=${width}${height}&bgset=bg${background}`;
-    
+    const { name, set, width, height, background } = this.state;
+    const uri =  `https://robohash.org/${name}?set=set${set}&size=${width}x${height}&bgset=bg${background}`;
+
     return (
       <div className="robot-component">
-        <input type="text" placeholder="John Doe" value={this.state.name}
-        <input type="range" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')} />
-        <input type="text" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')} />
-        <input type="range" value={this.state.height} min="100" max="500" onChange={this.updateRobot('height')} />
-        <input type="text" value={this.state.height} min="100" max="500" onChange={this.updateRobot('width')} />
-        <img alt="robot" src={this.uri}/>
+        <input type="text" placeholder="John Doe" value={this.state.name} onChange={this.updateRobot('name')}/>
+        <select onChange={this.updateRobot('set')}>
+          <option value="0" default>None</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+        <input type="range" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')}/>
+        <input type="number" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')}/>
+        <input type="range" value={this.state.height} min="100" max="500" onChange={this.updateRobot('height')}/>
+        <input type="number" value={this.state.height} min="100" max="500" onChange={this.updateRobot('width')}/>
+        <select onChange={this.updateRobot('background')}>
+          <option value="0" default>None</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+        {this.state.name === '' ? this.nameError() :  <img alt="robot" src={uri}/>}
       </div>
     );
   }

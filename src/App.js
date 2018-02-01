cdf9c17ccf54305a700f560ff45b254455864887
addriv <adrivero89@gmail.com>
2018-01-31 19:32:10 -0800

    Remove nameError function and move inside render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Robot',
+      name: 'Robot Joe',
       set: 1,
       width: 100,
       height: 100,
@@ -19,26 +19,28 @@ class App extends Component {
       this.setState({ [property]: event.target.value });
     };
   }
-
-  nameError() {
-    return (
-      <div className="name-error">Your robot needs a name!</div>
-    );
-  }
-
+  
   render() {
     const { name, set, width, height, background } = this.state;
     const uri =  `https://robohash.org/${name}?set=set${set}&size=${width}x${height}&bgset=bg${background}`;
+    const nameError = <div className="name-error">Your robot needs a name!</div>;
+    const robot = <img alt="robot" src={uri}/>;
 
     return (
       <div className="robot-component">
-        <input type="text" placeholder="John Doe" value={this.state.name} onChange={this.updateRobot('name')}/>
+        <input type="text" 
+          placeholder="John Doe" 
+          value={this.state.name} 
+          onChange={this.updateRobot('name')}/>
         <select onChange={this.updateRobot('set')}>
           <option value="0" default>None</option>
           <option value="1">1</option>
           <option value="2">2</option>
         </select>
-        <input type="range" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')}/>
+        <input type="range" 
+          value={this.state.width} 
+          min="100" max="500" 
+          onChange={this.updateRobot('width')}/>
         <input type="number" value={this.state.width} min="100" max="500" onChange={this.updateRobot('width')}/>
         <input type="range" value={this.state.height} min="100" max="500" onChange={this.updateRobot('height')}/>
         <input type="number" value={this.state.height} min="100" max="500" onChange={this.updateRobot('width')}/>
@@ -47,7 +49,7 @@ class App extends Component {
           <option value="1">1</option>
           <option value="2">2</option>
         </select>
-        {this.state.name === '' ? this.nameError() :  <img alt="robot" src={uri}/>}
+        {this.state.name === '' ? nameError : robot }
       </div>
     );
   }

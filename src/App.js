a03fa6ec938e25f495550edb877c8e0a0299aa19
addriv <adrivero89@gmail.com>
2018-01-31 23:42:48 -0800

    Testing filename change to resolve heroku error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Robot from './components/Robot';
-import './stylesheets/app.css';
+import './stylesheets/App.css';
 
 const App = () => {
   return (
